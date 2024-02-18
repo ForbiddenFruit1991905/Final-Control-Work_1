17c43a605885b6e2da15c923007292bb2c0dc101
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-02-18 18:07:24 +0300

    добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] array = new string [5] {"999", "***", "Sonya", "Linz", "art"};
+var new_array = new string[array.Length];
+
+int count = 0;
+for(int i=0; i < array.Length; i++)
+{
+if(array[i].Length <= 3)
+        {
+        new_array[count] = array[i];
+        count++;
+        }
+}
+Console.WriteLine("[" + string.Join(", ", new_array, 0, count) + "]"); 
